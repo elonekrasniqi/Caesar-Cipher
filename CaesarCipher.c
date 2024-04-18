2a76d1ff1a2772df5b9fa9e1d88b4ac9d5d07fc5
ElmaShabani <154560182+ElmaShabani@users.noreply.github.com>
2024-04-18 14:12:43 +0200

    CaesarCipher.c

--- CaesarCipher.c
+++ CaesarCipher.c
@@ -4,3 +4,19 @@
 
 using namespace std;
 
+string readPlainTextFromFile(const string& fileName) {
+    string result = "";
+    string line;
+    ifstream file(fileName);
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            result += line + "\n";
+        }
+        file.close();
+    }
+    else {
+        cout << "File not found: " << fileName << endl;
+    }
+    return result;
+}
+
